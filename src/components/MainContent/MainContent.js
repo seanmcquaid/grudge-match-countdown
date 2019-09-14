1e470c68e4daa2571a4280fbf4fd1fcf5dddf5f7
seanmcquaid <sean.mcquaid2@verizon.net>
2019-09-14 15:27:46 -0400

    starting timer work but wont take more than a day to work this out

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./MainContent.module.css";
+import Timer from "../../containers/Timer/Timer";
 
 const MainContent = props => {
     return (
         <div className={styles.mainContentContainer}>
-            
+            <Timer />
         </div>
     )
 }
--- src/containers/Timer/Timer.js
+++ src/containers/Timer/Timer.js
@@ -2,10 +2,19 @@ import React, {Component} from "react";
 
 class Timer extends Component {
     state = {
-        time : 0,
+        time : new Date()
     }
-    
+
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({
+                time : new Date()
+            })
+        }, 1000);
+    }
+
     render(){
+        console.log(this.state.time)
         return(
             <div>
                 Timer here
